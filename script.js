fe04c76b85aaf353197b5d21ff491b3a7f946988
Amanda Vitoria Leite da Silva <113052101+Dev-Amanda@users.noreply.github.com>
2022-10-21 10:15:32 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+function sendEmail() {
+    var params = {
+      name: document.getElementById("name").value,
+      email: document.getElementById("email").value,
+      phone: document.getElementById("phone").value,
+      message: document.getElementById("message").value,
+    };
+    const serviceID = "service_lurwq17";
+    const templateID = "template_lbekmqv";
+    
+    emailjs
+      .send(serviceID, templateID, params)
+      .then((res) => {
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("phone").value = "";
+        document.getElementById("message").value = "";
+        console.log(res);
+        alert("Sua mensagem foi enviada com sucesso");
+      })
+      .catch((err) => console.log(err));
+  }
+  
